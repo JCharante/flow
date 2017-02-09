e91a2e4e96dc85f6cd39305641db663a92477628
John <jcharante@gmail.com>
2017-02-09 09:46:12 -0500

    Fixed dashboard by updating it to use new route

--- client_server/static/internal/dashboard/js/script.js
+++ client_server/static/internal/dashboard/js/script.js
@@ -15,24 +15,35 @@ Vue.component('groups', {
 	mounted: function() {
 		var self = this;
 		getURLs(function() {
-			$.getJSON(urls.auth_server_address + '/users/' + getAID() + '/groups',
-				function (data) {
-					/*var alt_data = {
-						groups: [
-							{
-								name: 'TestGroup1',
-								owner: 'JCharante',
-								group_id: '3rnf2i3rj3'
-							},{
-								name: 'TestGroup2',
-								owner: 'JCharante',
-								group_id: '313123123'
-							}
-						]
-					};*/
-					self.$data.groups = data.groups;
+			var data = {
+				aid: getAID()
+			};
+
+			$.ajax({
+				method: 'POST',
+				url: urls.auth_server_address + '/users/groups',
+				data: JSON.stringify(data),
+				dataType: "json",
+				contentType: "application/json",
+				statusCode: {
+					200: function (data) {
+						/*var alt_data = {
+						 groups: [
+						 {
+						 name: 'TestGroup1',
+						 owner: 'JCharante',
+						 group_id: '3rnf2i3rj3'
+						 },{
+						 name: 'TestGroup2',
+						 owner: 'JCharante',
+						 group_id: '313123123'
+						 }
+						 ]
+						 };*/
+						self.$data.groups = data.groups;
+					}
 				}
-			);
+			});
 		});
 	},
 	template: '<div id="groups" class="row"><group v-for="group in groups" v-bind:name="group.name" v-bind:owner="group.owner" v-bind:group-id="group.group_id"></group></div>',
