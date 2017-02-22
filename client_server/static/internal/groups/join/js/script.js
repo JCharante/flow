782d48483d062f2a58833c2e258e4388139732b8
John <jcharante@gmail.com>
2017-02-22 16:10:07 -0500

    /groups/join on the website is now a page where you can enter an invite code to join a group. It still joins automatically with the correct GET queries

--- client_server/static/internal/groups/join/js/script.js
+++ client_server/static/internal/groups/join/js/script.js
@@ -1,24 +1,35 @@
 function main() {
+
+	if ($.QueryString.invite_code != null) {
+		joinGroup($.QueryString.invite_code)
+	} else {
+		$('#submit').click(function() {
+			joinGroup($('#invite_code').val())
+		})
+	}
+}
+
+function joinGroup(invite_code) {
 	var data = {
 		aid: getAID(),
-		invite_code: $.QueryString.invite_code
+		invite_code: invite_code
 	};
 
 	$.ajax({
-			method: 'POST',
-			url: urls.auth_server_address + '/groups/join',
-			data: JSON.stringify(data),
-			dataType: 'json',
-			contentType: 'application/json',
-			statusCode: {
-				200: function (data) {
-					redirectToDashboard()
-				},
-				400: function (data) {
-					redirectToDashboard()
-				}
+		method: 'POST',
+		url: urls.auth_server_address + '/groups/join',
+		data: JSON.stringify(data),
+		dataType: 'json',
+		contentType: 'application/json',
+		statusCode: {
+			200: function (data) {
+				redirectToDashboard()
+			},
+			400: function (data) {
+				redirectToDashboard()
 			}
-		})
+		}
+	})
 }
 
 function onceDocumentReady() {
