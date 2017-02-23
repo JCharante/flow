eabd40acbcc687a27ed3bf9f64bd5c558871e5b4
John <jcharante@gmail.com>
2017-02-22 19:42:03 -0500

    Added vue components for the queue. Can now see the queue and join it. There is a bug with the .toFrom call

--- client_server/static/internal/groups/group/js/script.js
+++ client_server/static/internal/groups/group/js/script.js
@@ -1,3 +1,91 @@
+Vue.component('queue-row', {
+	props: ['name', 'inQueueSince'],
+	computed: {
+		timeSinceInQueue: function(dateString) {
+			return moment.utc(dateString).toNow(true);
+		}
+	},
+	template:
+	'<tr>' +
+		'<td>{{ name }}</td>' +
+		'<td>{{ timeSinceInQueue }}</td>' +
+		'<td></td>' +
+	'</tr>'
+});
+
+Vue.component('queue', {
+	mounted: function() {
+		var self = this;
+		ws = new WebSocket(urls.websocket_server_address);
+
+		ws.onopen = function () {
+			ws.send(JSON.stringify({
+				aid: getAID()
+			}));
+		};
+
+		ws.onmessage = function (message) {
+			try {
+				var json = JSON.parse(message.data);
+				if (json.request === 'authentication update') {
+					ws.send(JSON.stringify({
+						request: 'switch_active_group',
+						group_id: $.QueryString.group_id
+					}))
+				} else if (json.request === 'switch active group confirmation') {
+					successAlert('Successfully Loaded Group', 1500)
+				} else if (json.request === 'queue update') {
+					console.log(json.in_queue);
+					self.$data.in_queue = json.in_queue;
+				}
+
+			} catch (e) {
+				console.log('This doesn\'t look like a valid JSON: ', message.data);
+			}
+
+		};
+
+		ws.onclose = function () {
+			errorAlert('Oh no our connection closed!', 1500);
+		};
+
+		$('#join-queue').click(function() {
+			ws.send(JSON.stringify({
+				request: 'join_queue'
+			}));
+			successAlert('Joined the Queue', 1500);
+		})
+	},
+	data: function() {
+		return {
+			in_queue: [
+				{
+					username: 'bob',
+					inQueueSince: '12am'
+				}
+			]
+		}
+	},
+	template:
+	'<div>' +
+		'<h3 class="center">Queue</h3>' +
+		'<table>' +
+			'<thead>' +
+				'<tr>' +
+					'<th data-field="id">Name</th>' +
+					'<th data-field="name">Waiting Since</th>' +
+					'<th data-field="price">Rank</th>' +
+				'</tr>' +
+			'</thead>' +
+
+			'<tbody>' +
+				'<queue-row v-for="ticket in in_queue" v-bind:name="ticket.username" v-bind:in-queue-since="ticket.inQueueSince"></queue-row>' +
+			'</tbody>' +
+		'</table>' +
+		'<a id="join-queue">Join Queue</a>' +
+	'</div>'
+});
+
 Vue.component('group-page', {
 	delimiters: ['[[', ']]'],
 	mounted: function() {
@@ -37,7 +125,7 @@ Vue.component('group-page', {
 			'<h3>[[ group.invite_code ]]</h3>' +
 		'</div>' +
 		'<div class="col l8">' +
-			'<h3 class="center">Queue</h3>' +
+			'<queue></queue>' +
 		'</div>' +
 	'</div>',
 	data: function () {
